335452f0400e18ce6de8f997cd00b315fbbabb0e
Karen Acosta <kar3n.acosta@gmail.com>
2021-01-09 23:41:17 -0600

    updating seed.sql file

---
+++ main/db/seed.sql
@@ -0,0 +1,34 @@
+use employees;
+
+INSERT INTO department
+  (name)
+VALUES
+  ('Sales'),
+  ('Engineering'),
+  ('Finance'),
+  ('Legal');
+
+INSERT INTO role
+  (title, salary, department_id)
+VALUES
+  ('Sales Lead', 102000, 1),
+  ('Salesperson', 85000, 1),
+  ('Lead Engineer', 150000, 2),
+  ('Software Engineer', 120000, 2),
+  ('Account Manager', 162000, 3),
+  ('Accountant', 125000, 3),
+  ('Legal Team Lead', 250000, 4),
+  ('Lawyer', 195000, 4);
+
+INSERT INTO employees
+  (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Sid', 'Bayou', 1, NULL),
+  ('Thomas', 'Landen', 2, NULL),
+  ('Tristan', 'Knight', 3, NULL),
+  ('Takari', 'Sauvignon', 4, NULL),
+  ('Isolde', 'Waterfall', 5, NULL),
+  ('Azuhre', 'Mando', 6, NULL),
+  ('Grogu', 'Doe', 7, NULL),
+  ('Alaya', 'Kingston', 8, NULL);
+
