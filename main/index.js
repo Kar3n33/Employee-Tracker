4ac36f89d01c82acad24d7f04893350a78147bff
Karen Acosta <kar3n.acosta@gmail.com>
2021-01-12 00:38:06 -0600

    changing structure of  application

---
+++ main/index.js
@@ -0,0 +1,23 @@
+require('dotenv/types').config();
+
+const mySQL = require("mysql");
+const inquirer = require("inquirer");
+const table = require("console.table");
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: process.env.MYSQL_PASSWORD,
+  database: 'employees_db',
+  });
+
+// connect to the mysql server and sql database
+connection.connect((err) {
+if (err) throw err;
+console.log('connected as id ' + connection.threadID);
+connection.end();
+});
+// run the start function after the connection is made to prompt the user
+
+
